bf8f373f48145ce843aa848057989959785626af
Marcílio Júnior <marciliojr@outlook.com>
2018-04-12 15:24:25 -0300

    Update Display7seg.v

--- Display7seg.v
+++ Display7seg.v
@@ -2,7 +2,7 @@ module Display7seg (key, char); 				//declaração do módulo
 input [2:0] key;						//declaração das entradas
 output reg [2:0] char;						//declaração das saídas
 
-always @(*) begin				
+always @ (*) begin				
 	case(key)						//declarações de caso para a variável de entrada
 		3'b000: char = 7'b0001001;			//relação posição da chave e resultado exibido	
 		3'b001: char = 7'b0000110;			
